17fbeb9530d1913dad39bb1b49d12c680bd560da
Cavan Wagg <waggx002@gmail.com>
2018-03-06 18:28:09 -0600

    finishing touches, var to let, variable name changes

--- game.js
+++ game.js
@@ -1,146 +1,196 @@
+$(document).ready(function() {
+  const wordBank = [
+    "bulbasaur",
+    "snorlax",
+    "jynx",
+    "squirtle",
+    "butterfree",
+    "pikachu",
+    "nidoqueen",
+    "nidoking",
+    "sandshrew",
+    "clefable",
+    "vulpix",
+    "oddish",
+    "venomoth",
+    "dugtrio",
+    "meowth",
+    "primeape",
+    "psyduck",
+    "growlith",
+    "poliwag",
+    "alakazam",
+    "machamp",
+    "weepinbell",
+    "tentacruel",
+    "geodude",
+    "rapidash",
+    "slowbro",
+    "magnemite",
+    "dodrio",
+    "grimer",
+    "cloyster",
+    "gengar",
+    "onix",
+    "drowzee",
+    "electrode",
+    "exeggutor",
+    "cubone",
+    "hitmonlee",
+    "lickitung",
+    "weezing",
+    "rhydon",
+    "chansey",
+    "tangela",
+    "starmie",
+    "scyther",
+    "magmar",
+    "tauros",
+    "lapras",
+    "ditto",
+    "vaporeon",
+    "porygon",
+    "aerodactyl",
+    "zapdos",
+    "dragonite",
+    "mewtwo"
+  ];
 
+  let wordArray = new Array();
+  let previousGuesses = new Array();
+  let currentWord = new String();
+  let livesRemaining;
 
-$(document).ready(function () {
-
-  //controller
-  // var alphabet = [
-  //   "a", "b", "c", "d",  "e", "f",  "g", "h", "i",  "j", "k", "l", "m", "n",  "o",  "p", "q",  "r",  "s", "t", "u", "v", "w", "x", "y", "z" ];
-  
-    const wordBank = ['bulbasaur', 'snorlax', 'jynx','squirtle','butterfree','pikachu','nidoqueen','nidoking','sandshrew','clefable','vulpix',
-    'oddish','venomoth','dugtrio','meowth','primeape','psyduck','growlith','poliwag','alakazam','machamp','weepinbell','tentacruel','geodude',
-    'rapidash','slowbro','magnemite','dodrio','grimer','cloyster','gengar','onix','drowzee','electrode','exeggutor','cubone','hitmonlee','lickitung',
-    'weezing','rhydon','chansey','tangela','starmie','scyther','magmar','tauros','lapras','ditto','vaporeon','porygon','aerodactyl','zapdos','dragonite',
-    'mewtwo'];
-  
-    var wordArray=new Array;
-    var previousGuesses=new Array;
-    var currentWord = new String;
-    var livesRemaining;
-  
   titleScreen();
   //title screen
-  function titleScreen(){
-    $('#gameContent').append('<div id="gameTitle"></div><div id="startButton" class="button">BEGIN</div>');		
-    $('#startButton').on("click",function (){gameScreen()});
-    $('#opening')[0].play();
-  }//display game
-  
-  
-    //display the game screen
-    
-    function gameScreen(){
+  function titleScreen() {
+    $("#content").append(
+      '<div id="gameTitle"></div><div id="BeginButton" class="button">BEGIN</div>'
+    );
+    $("#BeginButton").on("click", function() {
+      gameScreen();
+    });
+    $("#opening")[0].play();
+  } //display game
+
+  //display the game screen
+
+  function gameScreen() {
     livesRemaining = 7;
-    $('#gameContent').empty(); //empties titleScreen in order to create the gameScreen
-    $('#gameContent').append('<div id="wordHolder"></div>');
-    $('#gameContent').append('<div id="guesses">' + 'Previous guesses:' + '<br>' + '</div>');
-    $('#gameContent').append('<div id="lives">' + 'Lives Remaining: ' + livesRemaining + '</div>');
-    $('#gameContent').append('<div id="feedback"></div>');
-    
-  
+    $("#content").empty(); //empties titleScreen in order to create the gameScreen
+    $("#content").append('<div id="wordContainer"></div>');
+    $("#content").append(
+      '<div id="guesses">' + "Previous guesses:" + "<br>" + "</div>"
+    );
+    $("#content").append(
+      '<div id="lives">' + "Lives Remaining: " + livesRemaining + "</div>"
+    );
+    $("#content").append('<div id="playerFeedback"></div>');
+
     //for the button
     getWord();
-    var numberOfTiles= wordArray.length;
-    
-    previousGuesses=[];
-  
-    for(i=0; i<numberOfTiles; i++){
-      $('#wordHolder').append('<div class="tile" id=t'+i+'></div>');
+    let numberOfTiles = wordArray.length;
+
+    previousGuesses = [];
+
+    for (i = 0; i < numberOfTiles; i++) {
+      $("#wordContainer").append('<div class="tile" id=t' + i + "></div>");
     }
-  
-    $(document).on("keyup",handleKeyUp);
+
+    $(document).on("keyup", handleKeyUp);
   }
-  
+
   function getWord() {
-      currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-      wordArray = currentWord.split("");
-    }
-  
-  //Key input function 
-  $(document).on("keyup",handleKeyUp);
+    currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    wordArray = currentWord.split("");
+  }
+
+  //Key input function
+  $(document).on("keyup", handleKeyUp);
   function handleKeyUp(event) {
-  
-    if(event.keyCode>64 && event.keyCode<91){
-      var found=false; //this variable is used to determine if the typed key is located in the word
-      var previouslyEntered=false; //this variable is used to check if the letter has previously been typed
-      var input=String.fromCharCode(event.keyCode).toLowerCase(); //converts the code to a letter converting to lowercase
-      
-    
-      for(i=0;i<previousGuesses.length;i++){
-        if(input==previousGuesses[i]){previouslyEntered=true;}}
-          
-        
-      //only run if letter has not already been typed  
-      if(!previouslyEntered){
+    if (event.keyCode > 64 && event.keyCode < 91) {
+      let found = false; //this variable is used to determine if the typed key is located in the word
+      let previouslyEntered = false; //this variable is used to check if the letter has previously been typed
+      let input = String.fromCharCode(event.keyCode).toLowerCase(); //converts the code to a letter converting to lowercase
+
+      for (i = 0; i < previousGuesses.length; i++) {
+        if (input == previousGuesses[i]) {
+          previouslyEntered = true;
+        }
+      }
+
+      //only run if letter has not already been typed
+      if (!previouslyEntered) {
         previousGuesses.push(input); //adds the current letter to previousGuesses array.
         //check whether word contains the letter
-        for(i=0;i<wordArray.length;i++){
+        for (i = 0; i < wordArray.length; i++) {
           //if true, append letter to appropriate tile
-          if(input==wordArray[i]){found=true;$('#t'+i).append(input);}	
-        }//for
-          
-        if(found){checkAnswer();}
-        else{wrongAnswer(input);}
+          if (input == wordArray[i]) {
+            found = true;
+            $("#t" + i).append(input);
+          }
+        } //for
+
+        if (found) {
+          checkAnswer();
+        } else {
+          wrongAnswer(input);
+        }
       }
     }
   }
-  //check for answer 
-  function checkAnswer(){
-    var currentAnswer="";	
-    for(i=0;i<wordArray.length;i++){
-      currentAnswer+=($('#t'+i).text());
-    }		
-    if(currentAnswer==currentWord){
+  //check for answer
+  function checkAnswer() {
+    let currentAnswer = "";
+    for (i = 0; i < wordArray.length; i++) {
+      currentAnswer += $("#t" + i).text();
+    }
+    if (currentAnswer == currentWord) {
       victoryMessage();
-    };
-  };
+    }
+  }
   //For incorrect guesses
-  function wrongAnswer(a){
+  function wrongAnswer(a) {
     livesRemaining--;
-    $('#guesses').append(" "+a);
-    $('#lives').html('lives Remaining: ' + livesRemaining);
-    if(livesRemaining == 0){
+    $("#guesses").append(" " + a);
+    $("#lives").html("lives Remaining: " + livesRemaining);
+    if (livesRemaining == 0) {
       gameOver();
     }
   }
-  //Victory 
-  
-  function victoryMessage(){
+  //Victory
+
+  function victoryMessage() {
     document.activeElement.blur();
     $(document).off("keyup", handleKeyUp);
-    $('#feedback').append("You Win!!!<br><br><div id='replay' class='button'>CONTINUE</div>");
-    $('#opening')[0].pause();
-    $('#winSound')[0].play();
-    $('#replay').click(function(){
-      $('#winSound')[0].pause();
-      $('#opening')[0].play();
+    $("#playerFeedback").append(
+      "You Win!!!<br><br><div id='replay' class='button'>CONTINUE</div>"
+    );
+
+    $("#opening")[0].pause();
+    $("#winSound")[0].play();
+    $("#replay").click(function() {
+      $("#winSound")[0].pause();
+      $("#opening")[0].play();
       gameScreen();
-    }); 
+    });
   }
-  
-  
+
   //game over
-  function gameOver(){
-    //play sound effect
-    console.log("game over :(")
+  function gameOver() {
+    console.log("game over :(");
     $(document).off("keyup", handleKeyUp);
-    $('#feedback').append("Game Over!<br>(answer= "+ currentWord +")<div id='replay' class='button'>CONTINUE</div>");
-    $('#opening')[0].pause();
-    $('#failSound')[0].play();
-    $('#replay').click(function(){
-      $('#failSound')[0].pause();
-    $('#opening')[0].play();
+    $("#playerFeedback").append(
+      "Game Over!<br>(answer= " +
+        currentWord +
+        ")<div id='replay' class='button'>CONTINUE</div>"
+    );
+    $("#opening")[0].pause();
+    $("#failSound")[0].play();
+    $("#replay").click(function() {
+      $("#failSound")[0].pause();
+      $("#opening")[0].play();
       gameScreen();
-    }); 
-    
+    });
   }
-    // $('#replay').on("click",function (){
-    // 	if(questionBank.length>0){
-    // 		gameScreen()}
-    // 	else{finalPage()}
-    
-  //defeat
-  
-  
-   
-  
-  });
+});
